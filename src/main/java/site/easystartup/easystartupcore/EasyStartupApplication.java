311cf6c73a567308a66f4e73e33065e13cdd68e6
iRedTea <red__tea@outlook.com>
2022-08-26 23:28:48 +0300

    Initial commit

---
+++ src/main/java/site/easystartup/easystartupcore/EasyStartupApplication.java
@@ -0,0 +1,13 @@
+package site.easystartup.easystartupcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyStartupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyStartupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/easystartup/easystartupcore/EasyStartupApplicationTests.java
@@ -0,0 +1,13 @@
+package site.easystartup.easystartupcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyStartupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
